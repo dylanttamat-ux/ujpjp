b225d4fa85d936783336fd530776f5003d02e260
dylanttamat-ux <dylanttamat@mindjam.org.uk>
2026-09-16 15:41:02 +0100

    Updating to the version before I got hacked

--- javascript/javascript/javascript/todo.js
+++ javascript/javascript/javascript/todo.js
@@ -4,8 +4,8 @@ function title(titl) {
     return `<h1>${titl}</h1>`
 }
 
-function desc() {
-    return '<p>shdispd</p>'
+function desc(dsc) {
+    return `<p>${dsc}</p>`
 }
 
 function urgent() {
@@ -16,24 +16,27 @@ function date() {
     return '<br> To do by: <input type="date"></input>'
 }
 
-function card() {
-    const card = document.getElementById('card');
-
-    card.insertAdjacentHTML('beforeend', title("hlhbkljhkj"))
-    card.insertAdjacentHTML('beforeend', desc())
+function card(t, d) {
+    const card = document.createElement('div')
+    card.className = 'card'
+    card.insertAdjacentHTML('beforeend', title(t))
+    card.insertAdjacentHTML('beforeend', desc(d))
     card.insertAdjacentHTML('beforeend', urgent())
     card.insertAdjacentHTML('beforeend', date())
+
+    return card
 }
 
 function add() {
     // Create new divs (for new card)
     // Get new values for each div
     // Assign values to card 
-    const dv = document.createElement('div')
-    dv.className = 'card'
-    return dv;
+    const t = document.getElementById('ti').value;
+    const d = document.getElementById('de').value;
+    const newCard = card(t, d)
+    const cardList = document.getElementById('cards')
+
+    cardList.appendChild(newCard)
+
 }
 
-card()
-card()
-add()
